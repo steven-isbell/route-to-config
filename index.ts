dfca389a781d562f897578754a86337f2c9dc272
Steven Isbell <sisbell@sofi.org>
2019-07-31 08:50:05 -0600

    adding options

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
-import program from 'commander'
- 
+import program from "commander";
+
 program
-  .version('0.0.1')
-  .option('-f, --file [file]', 'Add a specified file')
+  .version("0.0.1")
+  .option("-s, --source [file]", "The file to convert.")
+  .option(
+    "-n, --output-file [fileName]",
+    "The name of the output file. Defaults to routes.js."
+  )
+  .option(
+    "-p, --path",
+    "The output path. Defaults to the directory command was executed in."
+  )
   .parse(process.argv);
- 
+
+export default function main({ source }) {
+  try {
+  } catch (e) {
+    console.error(e.message);
+    program.outputHelp();
+    process.exit(2);
+  }
+}
