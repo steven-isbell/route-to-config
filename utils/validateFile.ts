fc478127b60dfaff70e45a30509e8d0a7537cb92
Steven Isbell <sisbell@sofi.org>
2019-08-02 12:39:47 -0600

    add validateFile method

--- utils/validateFile.ts
+++ utils/validateFile.ts
@@ -1,7 +1,11 @@
 import fs from 'fs';
+import { VALID_EXTENSIONS } from '../constants';
 
-function validateFile(filePath) {
-  const extension = filePath.slice().split('.').pop();
+function validateFile(filePath: string) {
+  const extension = filePath.slice().split('.').pop() || '';
   const exists = fs.existsSync(filePath);
-  const isValidExtension = 
+  const isValidExtension = VALID_EXTENSIONS.includes(extension);
+  return exists && isValidExtension;
 }
+
+export default validateFile;
